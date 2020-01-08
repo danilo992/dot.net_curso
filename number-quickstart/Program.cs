e7d73a2e1afbace2d845d9242e0ca2f2fa58f96b
You Name <tadeudanilo46@gmail.com>
2020-01-08 09:37:16 -0300

    instalando framework .NET, e fazendo programa com números inteiros com c#.

---
+++ number-quickstart/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumbersInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+
+            int soma = 10 +10;
+            int sub = 10 * 10;
+            int div = 10 / 10;
+
+            
+           
+            Console.WriteLine(soma);
+            Console.WriteLine(sub);
+            Console.WriteLine(div);
+            Console.WriteLine(c);
+        }
+    }
+}
